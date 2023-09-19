0e3c32b42066082499bc8bf8f5bbe7b85232557c
khushboosharma2003 <khushboo.261203@gmail.com>
2023-09-19 21:41:45 +0530

    homedone

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const mainElement = document.getElementById("main");
 console.log(mainElement.textContent)
 mainElement.style.transition='.5s'
 mainElement.addEventListener("mouseover", () => {
-    mainElement.style.color='rgb(198, 34, 243)'
+    mainElement.style.color='rgb(207, 118, 232)'
     mainElement.textContent = "Technical Projects";
 
 });
