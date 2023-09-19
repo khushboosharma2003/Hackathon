88e22272ccd5270478c68a5ba33a95d34987f9ab
sambhavmishra3012 <106074009+sambhavmishra3012@users.noreply.github.com>
2023-09-19 20:21:46 +0530

    sambhav ne kiye changes

--- index.php
+++ index.php
@@ -13,20 +13,34 @@
 <body>
     <div>
         <nav>
-            <label for="check" id="foodvilla">TECH PRO</label>
+            <label for="check" class="techpro" id="main">TechPro</label>
             <input type="checkbox" name="check" id="check" />
             <label for="check" class="checkbtn">
                 <i class="fa-solid fa-bars checkbtn" style="color: white;"></i>
             </label>
 
             <ul>
-                <li class="navli"><a href="/">Home</a></li>
-                <li class="navli"><a href="/">About</a></li>
-                <li class="navli"><a href="/">Projects</a></li>
-                <li class="navli"><a href="/">LOGIN</a></li>
-                <li class="navli"><a href="/">SIGN UP</a></li>
+                <li class="navli"><a href="#home">Home</a></li>
+                <li class="navli"><a href="#about">About</a></li>
+                <li class="navli"><a href="#projects">Projects</a></li>
+                <li class="navli"><a href="#login">LOGIN</a></li>
+               
             </ul>
         </nav>
     </div>
+
+    <div class="quote_photo">
+
+<h1 class="small_quote">
+    Infinite Ideas, One Platform
+</h1> 
+<div class="bde_quote">
+    Welcome to our hub of innovation, where students from diverse universities unite to showcase their brilliance. Explore, collaborate, and be inspired by the endless possibilities that knowledge sharing brings
+</div>
+<img src="https://media.geeksforgeeks.org/wp-content/uploads/20200827094245/TipsforNonCSITStudentstoGetIntoComputerScienceField.jpg" alt="image" srcset="" id="img-home"> 
+</div>
+
+
+    <script src="script.js"></script>
 </body>
 </html>
