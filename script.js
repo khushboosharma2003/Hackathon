3c3b4478c70b61796eaf33c91115e585a408be11
khushboosharma2003 <khushboo.261203@gmail.com>
2023-09-19 19:33:34 +0530

    navdone

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 const mainElement = document.getElementById("main");
 console.log(mainElement.textContent)
-mainElement.style.transition = '0.5s';
+mainElement.style.transition='.5s'
 mainElement.addEventListener("mouseover", () => {
+    mainElement.style.color='rgb(198, 34, 243)'
     mainElement.textContent = "Technical Projects";
+
 });
 mainElement.addEventListener("mouseout", () => {
     mainElement.textContent = "TechPro";
+    mainElement.style.color='white'
 });
